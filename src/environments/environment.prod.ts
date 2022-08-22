0a94a625e7b6bd8a7492c948299ee64758b6165b
OrkhanDede <vasoft01@gmail.com>
2022-08-22 04:58:58 -0700

    prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  domain: 'https://demo-game-server.azurewebsites.net/',
+  host: 'https://demo-game-server.azurewebsites.net/',
+  apiUrl: 'https://demo-game-server.azurewebsites.net//api',
 };
